e3a77d43f172bb8b15cbd6ba6db7a34e546410df
Justin Kasteleijn <justin.kasteleijn@faber-electronics.nl>
2024-01-12 13:33:05 +0100

    Added easier to understand API calls

--- Tenma PowerSupply API/API/PowerAPI.vb
+++ Tenma PowerSupply API/API/PowerAPI.vb
@@ -5,13 +5,28 @@ Imports Tenma_PowerSupply_API.Tenma.Commands
 
 Namespace Tenma
     Partial Public Class API
-        Public Shared Function WritePowerState(conn As SerialPort, state As WriteDevicePowerStateCommand) As Result(Of Output, String)
+        Private Shared Function WritePowerState(conn As SerialPort, state As WriteDevicePowerStateCommand) As Result(Of Output, String)
             Return OpenConnection(conn).
                         AndThen(Function(unused) SendData(conn, state)).
                         Apply(Sub(unused) Threading.Thread.Sleep(20)).
                         AndThen(Function(unused) ReadStatus(conn)).
                         Map(Function(deviceStatus) deviceStatus.Output)
         End Function
+
+        Public Shared Function TurnOn(conn As SerialPort) As Result(Of Output, String)
+            Return WritePowerState(
+                conn,
+                New WriteDevicePowerStateCommand With {.State = Output.ON}
+            )
+        End Function
+
+        Public Shared Function TurnOff(conn As SerialPort) As Result(Of Output, String)
+            Return WritePowerState(
+                conn,
+                New WriteDevicePowerStateCommand With {.State = Output.OFF}
+            )
+        End Function
+
 
         Public Shared Function ReadPowerstate(conn As SerialPort) As Result(Of Output, String)
             Return ReadStatus(conn).
