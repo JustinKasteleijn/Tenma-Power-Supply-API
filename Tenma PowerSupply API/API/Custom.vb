4405d13e42934b3f8f86cdd6ec14d67d44ffa947
Justin Kasteleijn <justin.kasteleijn@faber-electronics.nl>
2024-01-15 08:56:57 +0100

    Added test connection method and CompatibilityTest method

---
+++ Tenma PowerSupply API/API/Custom.vb
@@ -0,0 +1,23 @@
+﻿Imports System.IO.Ports
+Imports FunctionalExtensions.Functional
+Imports Tenma_PowerSupply_API.Tenma.Commands
+
+Namespace Tenma
+    Partial Public Class API
+        Public Shared Function CompatibilityTest(conn As SerialPort, id As DeviceID) As Result(Of Boolean, String)
+            Return OpenConnection(conn).
+                AndThen(Function(unused) ReadDeviceID(conn)).
+                Assert(
+                    Function(data) data.Equals(id),
+                    Function(data) $"Data {data} is not identical to {id}, please ensure the function is working properly and the entered data is correct."
+                ).
+                Map(Function(unused) True)
+        End Function
+
+        Public Shared Function TestConnection(conn As SerialPort) As Result(Of DeviceStatus, String)
+            Return OpenConnection(conn).
+                    AndThen(Function(unused) ReadStatus(conn)).
+                    Apply(Sub(unused) conn.Close())
+        End Function
+    End Class
+End Namespace
