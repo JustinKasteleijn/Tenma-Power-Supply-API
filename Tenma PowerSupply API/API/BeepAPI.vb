dc80c9bedc79f21cadf83aaf30a231c5d9a48ded
Justin Kasteleijn <justin.kasteleijn@faber-electronics.nl>
2024-01-15 08:46:13 +0100

    Added the last library methods

---
+++ Tenma PowerSupply API/API/BeepAPI.vb
@@ -0,0 +1,14 @@
+﻿Imports System.IO.Ports
+Imports FunctionalExtensions.Functional
+Imports Tenma_PowerSupply_API.Tenma.Commands
+
+Namespace Tenma
+    Partial Public Class API
+        Public Shared Function Beep(conn As SerialPort, command As WriteBeepStateCommand) As Result(Of Output, String)
+            Return OpenConnection(conn).
+                AndThen(Function(unused) SendData(conn, command)).
+                AndThen(Function(unused) ReadStatus(conn)).
+                Map(Function(id) id.Beep)
+        End Function
+    End Class
+End Namespace
---
+++ Tenma PowerSupply API/Commands/Device/WriteBeepStateCommand.vb
@@ -0,0 +1,12 @@
+﻿Namespace Tenma
+    Namespace Commands
+        Public Structure WriteBeepStateCommand
+            Implements TenmaSerializable
+            Public State As Output
+
+            Public Function ToCommand() As String Implements TenmaSerializable.ToCommand
+                Return $"OCP{CInt(State)}"
+            End Function
+        End Structure
+    End Namespace
+End Namespace
